4eaeb0e68d23adb8733ef58df78113f227400333
Nicholas Guerrero <nicholasguerrero@crc-dot1x-nat-10-239-163-10.bu.edu>
2024-04-25 18:11:23 -0400

    added some comments at start

--- food.py
+++ food.py
@@ -16,33 +16,44 @@ from sklearn.metrics import accuracy_score,roc_auc_score, precision_score
 from torch_geometric.utils import negative_sampling
 import matplotlib.pyplot as plt
 
+#load the data
 edge_url="https://raw.githubusercontent.com/lamypark/FlavorGraph/master/input/edges_191120.csv"
 edges_df=pd.read_csv(edge_url)
 node_url="https://raw.githubusercontent.com/lamypark/FlavorGraph/master/input/nodes_191120.csv"
 nodes_df=pd.read_csv(node_url)
 
+#only keep ingredient related items
 edges_df = edges_df[edges_df['edge_type'] == 'ingr-ingr']
 nodes_df = nodes_df[nodes_df['node_type'] == 'ingredient']
 
-flavorGraph = Data()
+#initialize data object
+flavorGraph = Data() 
 
+#create 1xM tensor of the edge weights
 edge_weight = torch.tensor(edges_df['score'], dtype=torch.float)
+#create Nx1 tensor of the original node IDs
 node_index = torch.tensor(nodes_df['node_id'], dtype=torch.int64).unsqueeze(1)
 
+#give every node an index
 node_map = dict()
 for i in range(len(node_index)):
     node_map[(int(node_index[i]))] = i
 
+#convert edges into 2xM tensor where each column is an edge, and the 2 rows are the nodes involved
+#initially make it as a 2D array
 edge_index_np = np.array([
     edges_df.id_1.apply(lambda x: node_map[x]).values,
     edges_df.id_2.apply(lambda x: node_map[x]).values
 ])
+#converts the 2D array into a tensor
 edge_index = torch.as_tensor(edge_index_np, dtype=torch.long)
 
-flavorGraph.x = node_index # 2D Tensor
-flavorGraph.edge_index = edge_index # 2D Tensor
-flavorGraph.edge_weight = edge_weight # 1D Tensor
+#populate our Data() object
+flavorGraph.x = node_index # Nx1 tensor of original node IDs
+flavorGraph.edge_index = edge_index # 2xM tensor of edges (ingredient relationships)
+flavorGraph.edge_weight = edge_weight # 1xM tensor of edge weights
 
+#split FlavorGraph into training, validation, and test datasets
 transform = RandomLinkSplit(is_undirected=True,add_negative_train_samples=False,disjoint_train_ratio=0.35)
 train_data, val_data, test_data = transform(flavorGraph)
 
@@ -133,7 +144,7 @@ final_edge_probs_GCN = model.decode_all(z)
 #plotting results
 plt.plot(np.arange(len(validationMetrics_GCN)),np.array(validationMetrics_GCN)[:,1],label='test_auc')
 plt.legend()
-"""plt.savefig('/Users/nicholasguerrero/Desktop/Coursework/CS365/GNN-Food-Pairing/plot1.png')"""
+#plt.savefig('/Users/nicholasguerrero/Desktop/Coursework/CS365/GNN-Food-Pairing/plot1.png')
 plt.show()
 
 start_node = np.random.randint(0, 6653)
@@ -227,7 +238,7 @@ final_edge_probs_SAGE = model.decode_all(z)
 # Graphing results
 plt.plot(np.arange(len(validationMetrics_SAGE)),np.array(validationMetrics_SAGE)[:,1],label='test_auc')
 plt.legend()
-"""plt.savefig('/Users/nicholasguerrero/Desktop/Coursework/CS365/GNN-Food-Pairing/plot2.png')"""
+#plt.savefig('/Users/nicholasguerrero/Desktop/Coursework/CS365/GNN-Food-Pairing/plot2.png')
 plt.show()
 start_node = np.random.randint(0, 6653)
 for i in range(1, min(np.random.poisson(4, 1)[0] + 1, 7)):
@@ -240,5 +251,5 @@ for i in range(1, min(np.random.poisson(4, 1)[0] + 1, 7)):
 plt.plot(np.arange(len(validationMetrics_GCN)),np.array(validationMetrics_GCN)[:,1],label='test_auc_GCN')
 plt.plot(np.arange(len(validationMetrics_SAGE)),np.array(validationMetrics_SAGE)[:,1],label='test_auc_SAGE')
 plt.legend()
-"""plt.savefig('/Users/nicholasguerrero/Desktop/Coursework/CS365/GNN-Food-Pairing/plot3.png')"""
+#plt.savefig('/Users/nicholasguerrero/Desktop/Coursework/CS365/GNN-Food-Pairing/plot3.png')
 plt.show()
